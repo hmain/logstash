bf5e1ec0720e5723e36138a3fada6e03ef21ff98
Hamin Mousavi <hmo@zmags.com>
2014-02-21 13:21:42 +0100

    A bit more verbose for unpacking and importing

--- s3_to_logstash.sh
+++ s3_to_logstash.sh
@@ -5,16 +5,37 @@
 # Edit this
 S3_CONFIG="/root/.s3cfg"
 BUCKET="s3://bucket.name"
+LOCAL_GZIP="/mnt/cloudfront_gzip"
 GZIP_PATH="/mnt/gzip"
-LOG_PATH="/mnt/logs"
-
-# Sync data from bucket to localhost
-/usr/bin/s3cmd -c $S3_CONFIG sync $BUCKET $GZIP_PATH
-
-# Find and copy over the gzipped files, unpack them
-find $GZIP_PATH -type f -mtime -30m -exec cp {} /mnt/nextgen_logs/ \;
-find $LOG_PATH -type f -name "*.gz" -exec gunzip -f '{}' \;
+S3_GZIP_LOG="/mnt/s3_gzip.log"
+LOCAL_GZIP_LOG="/mnt/local_gzip.log"
+COMPARE_LOG="/mnt/compare.log"
+GZIP_DOWNLOAD_LOG="/mnt/download_gzip.log"
+GZIP_DOWNLOADED="/mnt/downloaded.log"
+DONE="/mnt/done.log"
 
 # Remove old logs
-find $LOG_PATH -type f -mtime +30 -exec rm -f '{}' \;
+rm -f $S3_GZIP_LOG $LOCAL_GZIP_LOG $COMPARE_LOG $GZIP_DOWNLOAD_LOG $GZIP_DOWNLOADED $DONE
 
+# List what's on localhost and what's on s3
+/usr/bin/s3cmd -c $S3_CONFIG ls $BUCKET | cut -d/ -f 5 | sort >> $S3_GZIP_LOG
+find $LOCAL_GZIP -type f -name "*.gz" | cut -d/ -f 4 | sort >> $LOCAL_GZIP_LOG
+
+# Compare the lists and ignore what's on local
+comm --nocheck-order -23 $S3_GZIP_LOG $LOCAL_GZIP_LOG >> $COMPARE_LOG
+
+# Add the s3 path to beginning of each line
+perl -pe "print '$BUCKET'" $COMPARE_LOG >> $GZIP_DOWNLOAD_LOG
+
+# Download what's missing from s3 in parallel
+# -j10 = 10 concurrent threads
+# -N1 = 1 argument from stdin, assigned to {1}
+# {1} = that argument (The URL from the logsfile)
+# Get parallel from http://www.gnu.org/software/parallel/
+cat $GZIP_DOWNLOAD_LOG | parallel -j10 -N1 --progress --no-notice /usr/bin/s3cmd -c $S3_CONFIG --no-progress get {1} $GZIP_PATH >> $GZIP_DOWNLOADED
+
+# Make a list of downloaded files
+cat $GZIP_DOWNLOADED | awk '{print $5}' | sed -e s/"'"/""/g >> $DONE
+
+# Uncomment to remove downloaded S3 files from S3. Adding expiry rule is prefered instead. 
+# cat $GZIP_DOWNLOAD_LOG | parallel -j10 -N1 --no-notice /usr/bin/s3cmd -c $S3_CONFIG --no-progress del {1}
